d22d0737c08a9ee6d4ee591aa0824656789db19c
J <silfos@gmail.com>
2022-09-12 11:41:48 +0100

    add static json file

--- netlify/functions/meme.ts
+++ netlify/functions/meme.ts
@@ -1,9 +1,36 @@
-import { Handler } from '@netlify/functions';
+import { Handler } from "@netlify/functions";
+import fetch from "isomorphic-fetch";
+
+let imgUrl = "https://cdn.jlopes.eu/memes/21tqf4.jpg";
 
 const handler: Handler = async (event, context) => {
+  const { fileURL } = event.queryStringParameters;
+
+  let image;
+  try {
+    const result = await fetch(imgUrl, {
+      headers: {
+        Authorization: `Bearer ${process.env.AUTH_TOKEN}`,
+      },
+    });
+    image = await result.buffer();
+  } catch (error) {
+    console.log("error", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message,
+      }),
+    };
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World' }),
+    headers: {
+      "Content-type": "image/jpeg",
+    },
+    body: image.toString("base64"),
+    isBase64Encoded: true,
   };
 };
 
