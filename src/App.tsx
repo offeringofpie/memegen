3a32b0b22b316edb06f44499eef02e2f6eea945e
J <silfos@gmail.com>
2022-09-16 22:51:46 +0100

    fix image reloading issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,13 @@
-import { useState } from 'react';
-import store from './store';
-import memeList from './memes.json';
+import { useState } from "react";
+import store from "./store";
+import memeList from "./memes.json";
 
-import MemeContainer from './components/MemeContainer';
-import List from './components/List';
-import TextBoxes from './components/TextBoxes';
-import Buttons from './components/Buttons';
+import MemeContainer from "./components/MemeContainer";
+import List from "./components/List";
+import TextBoxes from "./components/TextBoxes";
+import Buttons from "./components/Buttons";
 
-import './App.css';
-
-const draw = (ctx: CanvasRenderingContext2D) => {
-  const image = new Image();
-  const state = store.getState();
-
-  image.crossOrigin = 'anonymous';
-  ctx.imageSmoothingEnabled = true;
-
-  if (state) {
-    image.src = state.meme.url;
-
-    image.onload = () => {
-      ctx.canvas.width = state.meme.width;
-      ctx.canvas.height = state.meme.height;
-      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-      ctx.drawImage(
-        image,
-        0,
-        0,
-        state.meme.width,
-        state.meme.height,
-        0,
-        0,
-        ctx.canvas.width,
-        ctx.canvas.height
-      );
-
-      ctx.textAlign = 'center';
-      ctx.textBaseline = 'middle';
-
-      if (state.meme.boxes) {
-        state.meme.boxes.forEach((box: any, i: number) => {
-          ctx.fillStyle = state.meme.color || '#FFFFFF';
-          ctx.strokeStyle = state.meme.stroke || '#000000';
-          ctx.font = state.meme.font
-            ? `${state.meme.font.size}px ${state.meme.font.family}`
-            : '48px Anton';
-          if (box.size) {
-            ctx.font = ctx.font.replace(ctx.font.split('px')[0], box.size);
-          }
-          let lines = state[`text${i}`].split('\n');
-          if (lines.length > 1) {
-            let fontSize = ctx.font.split('px')[0];
-            ctx.font = ctx.font.replace(
-              fontSize,
-              `${Math.max(
-                parseInt(fontSize) / 2,
-                (parseInt(fontSize) * 2) / lines.length
-              )}`
-            );
-          }
-          lines.forEach((line: string, i: number) => {
-            let text = state.meme.uppercase ? line.toUpperCase() : line;
-            const metrics = ctx.measureText(text);
-            const fontHeight =
-              metrics.actualBoundingBoxAscent +
-              metrics.actualBoundingBoxDescent;
-            ctx.fillStyle = box.color || ctx.fillStyle;
-            ctx.strokeStyle = box.stroke || ctx.strokeStyle;
-            ctx.save();
-            ctx.translate(
-              box.pos[0],
-              box.pos[1] + (10 + fontHeight) * i - fontHeight / 2
-            );
-            if (box.angle) {
-              ctx.rotate((box.angle * Math.PI) / 180);
-            }
-
-            ctx.shadowColor = ctx.strokeStyle as string;
-            ctx.shadowBlur = 5;
-            ctx.lineWidth = 2;
-            ctx.strokeText(text, 0, 0);
-            ctx.shadowBlur = 0;
-            ctx.fillText(text, 0, 0);
-            ctx.restore();
-          });
-        });
-      }
-    };
-  }
-};
+import "./App.css";
 
 function App() {
   const [state, setState] = useState(store.getState());
@@ -114,12 +33,11 @@ function App() {
         <div className="drawer-content flex flex-col items-center justify-center">
           <label
             htmlFor="drawer"
-            className="btn btn-circle btn-ghost drawer-button text-xl lg:hidden absolute top-2 left-2 z-40"
-          >
+            className="btn btn-circle btn-ghost drawer-button text-xl lg:hidden absolute top-2 left-2 z-40">
             &#9778;
           </label>
           <List list={memeList.data.memes} onChange={onChange} />
-          <MemeContainer draw={draw} />
+          <MemeContainer />
           <Buttons />
         </div>
         <div className="drawer-side">
@@ -127,8 +45,7 @@ function App() {
           <div className="menu p-4 overflow-y-auto w-80 bg-neutral-focus text-base-content relative">
             <label
               htmlFor="drawer"
-              className="btn btn-circle btn-ghost drawer-button lg:hidden text-lg absolute top-2 right-2 z-40"
-            >
+              className="btn btn-circle btn-ghost drawer-button lg:hidden text-lg absolute top-2 right-2 z-40">
               &times;
             </label>
             <TextBoxes boxes={state.meme?.boxes} />
--- src/components/MemeContainer/index.tsx
+++ src/components/MemeContainer/index.tsx
@@ -1,9 +1,96 @@
 import React from "react";
 import useCanvas from "./useCanvas";
+import store from "../../store";
+
 import style from "./style.module.css";
 
+const state = store.getState();
+const image = new Image();
+let currMeme = state.meme;
+
+const draw = (ctx: CanvasRenderingContext2D) => {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.imageSmoothingEnabled = true;
+  image.crossOrigin = "anonymous";
+
+  if (state) {
+    if (!image.src) {
+      image.src = state.meme.url;
+    }
+    if (state.meme !== currMeme) {
+      image.src = state.meme.url;
+      currMeme = state.meme;
+    }
+
+    image.onload = () => {
+      ctx.canvas.width = state.meme.width;
+      ctx.canvas.height = state.meme.height;
+    };
+    ctx.drawImage(
+      image,
+      0,
+      0,
+      state.meme.width,
+      state.meme.height,
+      0,
+      0,
+      ctx.canvas.width,
+      ctx.canvas.height
+    );
+    if (state.meme.boxes) {
+      state.meme.boxes.forEach((box: any, i: number) => {
+        ctx.fillStyle = state.meme.color || "#FFFFFF";
+        ctx.strokeStyle = state.meme.stroke || "#000000";
+        ctx.font = state.meme.font
+          ? `${state.meme.font.size}px ${state.meme.font.family}`
+          : "48px Anton";
+        if (box.size) {
+          ctx.font = ctx.font.replace(ctx.font.split("px")[0], box.size);
+        }
+        let lines = state[`text${i}`].split("\n");
+        if (lines.length > 1) {
+          let fontSize = ctx.font.split("px")[0];
+          ctx.font = ctx.font.replace(
+            fontSize,
+            `${Math.max(
+              parseInt(fontSize) / 2,
+              (parseInt(fontSize) * 2) / lines.length
+            )}`
+          );
+        }
+        lines.forEach((line: string, i: number) => {
+          let text = state.meme.uppercase ? line.toUpperCase() : line;
+          const metrics = ctx.measureText(text);
+          const fontHeight =
+            metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+          ctx.fillStyle = box.color || ctx.fillStyle;
+          ctx.strokeStyle = box.stroke || ctx.strokeStyle;
+          ctx.save();
+          ctx.translate(
+            box.pos[0],
+            box.pos[1] + (10 + fontHeight) * i - fontHeight / 2
+          );
+          if (box.angle) {
+            ctx.rotate((box.angle * Math.PI) / 180);
+          }
+
+          ctx.shadowColor = ctx.strokeStyle as string;
+          ctx.shadowBlur = 5;
+          ctx.lineWidth = 2;
+          ctx.strokeText(text, 0, 0);
+          ctx.shadowBlur = 0;
+          ctx.fillText(text, 0, 0);
+          ctx.restore();
+        });
+      });
+    }
+  }
+};
+
 const Canvas = (props: any) => {
-  const { draw, ...rest } = props;
+  const { ...rest } = props;
   const canvasRef = useCanvas(draw);
 
   return (
